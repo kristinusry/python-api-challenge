41ef24657dc996a414510819f336ea3eb9baecb8
kristinusry <67609941+kristinusry@users.noreply.github.com>
2020-10-03 23:26:35 -0700

    Upload hw

---
+++ Vacationpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "f76ba44494f167e2354ad12bfa939f5b"
+
+# Google API Key
+g_key = "INSERT YOUR OWN"
---
+++ Weatherpy/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "f76ba44494f167e2354ad12bfa939f5b"
